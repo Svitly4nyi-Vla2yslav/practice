621f39d135945c595ee17a5ea4de7d1c25af4024
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-06-02 23:22:35 +0200

    lesson-1

---
+++ java-script.js
@@ -0,0 +1,34 @@
+// const apples = 300 ;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total);
+// const diff = apples - grapes;
+// console.log(diff)
+// let students = 100;
+// students = students + 50;
+// students += 50;
+// students *= 2;
+// console.log(students)
+// const value = 27.5;
+// console.log(Math.floor(value)); //27 округлює до цілого
+// console.log(Math.ceil(value)); // 28 округлює до верху
+// console.log(Math.round(value)) //28 просто округлює
+//? cкласти фразу в один рядок з 3 змінних
+// const companyName = `Syberdyne System`;
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${companyName} has ${repairBots + defenceBots}` ;
+// //const message = companyName + ` has `  +(repairBots + defenceBots) + ` bots ` ;
+// console.log(message)
+//? індекс маси тіла необхідно обкруглити до однієї цифри після коми
+// let weight = `88,3`;
+// let height = `1.75`;
+// const newWeight = Number(weight.replace(',', '.'));
+// console.log(newWeight)
+// const newHeight = Number(height.replace(',', '.'))
+// console.log(newHeight)
+// // console.log(typeof Number(weight));
+// // console.log(Number(weight))
+// const bmi = newWeight / Math.pow(newHeight, 2);
+// console.log(bmi.toFixed(1));
+//

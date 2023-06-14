24a72d2f1aceb1ab0a648b9d6a4807ee56dcf0d5
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-06-14 23:38:57 +0200

    стрілочні функції

--- modul/4.js
+++ modul/4.js
@@ -65,10 +65,10 @@
 //     };
 //     return filteredArray;
 // };
-// // ? 1. Надо передать функцію 
-// // ? 2. Функція получає елемент масива
-// // ? 3. Якщо елемент масива задовільняє умову то функція поверне 'true'.
-// // ? 4. Якщо елемент масива НЕ задовільняє умову то функція поверне 'false'
+// ? 1. Надо передать функцію
+// ? 2. Функція получає елемент масива
+// ? 3. Якщо елемент масива задовільняє умову то функція поверне 'true'.
+// ? 4. Якщо елемент масива НЕ задовільняє умову то функція поверне 'false'
 // const callback1 = function (value) {
 //     return value >=3;
 // }
@@ -105,3 +105,170 @@
 // const fnB = fnA();
 // fnB();
 // console.log(fnB);
+// ? ---------------------Кухар---------------------------------------------------
+// const makeDish = function (sheffName, dish){
+//     console.log(`${sheffName} готує ${dish}`)
+// };
+// makeDish('Mango', '🥧');
+// makeDish('Mango', '🥚');
+// makeDish('Mango', '🍵');
+
+// makeDish('Poly', '🍽️');
+// makeDish('Poly', '☕');
+// makeDish('Poly', '🍔');
+// const makeSheff = function (name){
+//     const makeDish = function (dish){
+//         console.log(`${name} готує ${dish}`)
+//     };
+//     return makeDish;
+// };
+// const mango = makeSheff('Mango');
+// const poly = makeSheff('Poly');
+// mango('🥧')
+// mango('🥚')
+// poly('☕')
+// ?-------- Округлятор----🤷‍♂️-----------------
+
+// const floatingPoint = 3.456789;
+// const someInt = Math.round(floatingPoint);
+// const withDecimals = Number(floatingPoint.toFixed(2));
+// console.log(Number(floatingPoint.toFixed(3)));
+
+// const number1 = 3.4775;
+// const number2 = 4.98889;
+// console.log(Number(number1.toFixed(2)));
+// console.log(Number(number2.toFixed(4)));
+
+// const rounder = function (number, place) {
+//   return Number(number.toFixed(place));
+// };
+// console.log(rounder(2.343344, 3));
+// console.log(rounder(4.7676768585, 4));
+// console.log(rounder(9.8984949, 5))
+// console.log(rounder(1.9799, 2));
+
+// const rounder = function (place) {
+//   return function (number) {
+//     return Number(number.toFixed(place));
+//   };
+// };
+// const rounder2 = rounder(2);
+// const rounder3 = rounder(3);
+// console.log(rounder2(3.858858));
+// console.log(rounder3(3.858858));
+// console.log(rounder2(3.858858, 2));
+// console.log(rounder3(3.858858, 3));
+
+// ? Особисті дані і функції - приховані реалізації. Інтерфейс
+
+// const salaryManagerFactory = function (employeeName, baseSalary = 0) {
+//   let salary = baseSalary;
+
+//   const changeBy = function (amount) {
+//     salary += amount;
+//   };
+//   return {
+//     raise(amount) {
+
+//       if (amount > 5000){
+//         return "А ти батенька не зажрався?😄"
+//       }
+//       salary += amount;
+//     },
+//     lower(amount) {
+//         salary -= amount;
+//     },
+//     current() {
+//       return `Поточна зарплатa ${employeeName} - ${salary}$`;
+//     },
+//   };
+// };
+// const salaryManager = salaryManagerFactory('Vlad', 5000);
+// console.log(salaryManager.raise(5001))
+// console.log(salaryManager.current());
+
+//? Стрілочні функції ---------------------------------------------------
+//? - Оголошення --------------------------------------------------------
+//? - Явне і не явне повернення -----------------------------------------
+//? - Аргументи ---------------------------------------------------------
+//? - Не явне повернення об`єкта ----------------------------------------
+
+// const add = function (a, b, c){
+//     console.log(a, b, c);
+//     return a + b + c;
+// };
+// console.log(add(5, 10, 15));
+
+// const addArrow = (a, b, c) => {
+//     console.log(a, b, c);
+//     return a + b + c;
+// };
+// console.log(addArrow(5, 10, 15));
+
+// const addArrow1 = (a, b, c) => a + b + c;
+// console.log(addArrow1(5, 10, 15));
+// ?---------------------------------------------------------------------
+
+//   const add = function (a, b, c){
+//     console.log(arguments);
+//     return a + b + c;
+// };
+// console.log(add(5, 10, 15));
+
+// const addArrow = (a, b, c) => {
+
+//     return a + b + c;
+// };
+//    console.log(addArrow(5, 10, 15));
+// ? ====================================================================
+//  const fnA = function () {
+//     return {
+// a: 5,
+//     };
+//  };
+//  console.log(fnA());
+
+//  const fnB = () => ({arrowB: 5});
+//  console.log(fnB());
+// ?=====================================================================
+// const onGetPositionSucces = (position) => {
+//     console.log(position);
+// };
+// const onGetPositionError = (error) => {
+//   console.log(error);
+// };
+// window.navigator.geolocation.getCurrentPosition(
+//     onGetPositionError,
+//     onGetPositionSucces,
+// );
+// ?================================================================
+
+// const callback1 = function (value) {
+//     return value >=3;
+// }
+// const result1 = filter([1, 2, 3, 4, 5], callback1);
+
+// console.log(result1);
+// const callback2 = function (value){
+//     return value <= 4;
+// };
+// const result2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
+
+// console.log(result2);
+
+// const fruits = [
+//    { name: '🍎', quantity: 200, isFresh: true},
+//    { name: '🍇', quantity: 150, isFresh: false},
+//    { name: '🍌', quantity: 100, isFresh: true},
+// ];
+// const getFruitsWithQuantity = function (fruit) {
+//     return fruit.quantity >= 120;
+// };
+// const result3 = filter(fruits, getFruitsWithQuantity);
+// console.log(result3)
+// !!!!!!!   =>----------------------------------------------------------
+//* const getFruitsWithQuantity = fruit => fruit.quantity >= 120;        
+//*                                                                        
+
+//todo const result3 = filter(fruits, fruit => fruit.quantity >= 120);
+// console.log(result3)

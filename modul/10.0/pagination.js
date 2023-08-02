f68a21c3afb844cf3f80724440be26c02921ee43
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-08-02 12:21:38 +0200

    add

--- modul/10.0/pagination.js
+++ modul/10.0/pagination.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_VhS0gIDTmgOPHiWEPwELZWfTm5nPqfaEnwVhJII0n05SOylY1t3qrzW4fHoO3EVX';
+axios.defaults.headers.common['x-api-key'] =
+  'live_VhS0gIDTmgOPHiWEPwELZWfTm5nPqfaEnwVhJII0n05SOylY1t3qrzW4fHoO3EVX';
